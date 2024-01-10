ce56871bd9159cec6922221047d398ac82fea12c
Rylan Jurgens <122000590+gatekeeperR2000@users.noreply.github.com>
2024-01-10 14:25:56 -0700

    Delete 01_HelloWorld directory

--- 01_HelloWorld/# Hello World Program.py
+++
@@ -1,6 +0,0 @@
-# Hello World Program
-
-# This serves as the first program most programmers ever create
-# and is a monumment to starting my journey through the world of Computer Programming
-
-print("Hello World")
