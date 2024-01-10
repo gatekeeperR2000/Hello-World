b401fe9f73d091ade3362ae90a432dce25126a53
Rylan Jurgens <122000590+gatekeeperR2000@users.noreply.github.com>
2024-01-10 14:29:14 -0700

    Add files via upload

---
+++ Hello World Program.py
@@ -0,0 +1,6 @@
+# Hello World Program
+
+# This serves as the first program most programmers ever create
+# and is a monumment to starting my journey through the world of Computer Programming
+
+print("Hello World")
